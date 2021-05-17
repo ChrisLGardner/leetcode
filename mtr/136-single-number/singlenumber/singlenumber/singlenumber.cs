17f801aa572d66047bfc85fba959a2c48812b502
Thomas Rayner <thmsrynr@outlook.com>
2021-05-17 10:04:07 -0700

    136-single-number

---
+++ mtr/136-single-number/singlenumber/singlenumber/singlenumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace singlenumber
+{
+    public class Solution
+    {
+        public int SingleNumber(int[] nums)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            foreach (int i in nums)
+            {
+                if (map.TryGetValue(i, out int count))
+                {
+                    map[i] = count + 1;
+                    continue;
+                }
+                map.Add(i, 1);
+            }
+
+            return map.FirstOrDefault(x => x.Value == 1).Key;
+        }
+    }
+}
